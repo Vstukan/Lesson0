0735afd31184225dc41d9a60f4c96d72ce38ab33
Vstukan <v.stukan14@gmail.com>
2024-07-01 08:43:08 +0300

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,12 @@
+example = 'Топинамбур'
+print(example[0])
+print(example[-1])
+print(example[5:10])
+print(example[::-1])
+print(example[1::2])
+example = 'Поликарбонат'
+print(example[0])
+print(example[-1])
+print(example[5:10])
+print(example[::-1])
+print(example[1::2])
