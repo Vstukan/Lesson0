85617465289f2a6b15f411f27b12b144c6cc689d
Vstukan <v.stukan14@gmail.com>
2024-07-02 08:34:05 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,4 @@
+homework_count = 12
+hour_count = 1.5
+name_curs = 'Python'
+print('Курс:', name_curs, ', всего задач:', homework_count, ', среднее время выполнения', hour_count / homework_count, 'часа')
