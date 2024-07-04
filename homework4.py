96603e9bf28412204135c71baaf28a57d021288a
Vstukan <v.stukan14@gmail.com>
2024-07-04 12:50:26 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,5 @@
+my_string = input('Название курсов: ')
+print('Количество символов: ', len(my_string))
+print(my_string.lower() , my_string.upper())
+print(my_string.replace(' ',''))
+print(my_string[0] , my_string[-1])
