acffafb6092d6bb2a32e5c74c271cf3f51112ea3
Vstukan <v.stukan14@gmail.com>
2024-07-04 15:11:18 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,6 @@
+immutable_var = 3, 'apple', 16, '12', True
+print('Immutable_var: ', immutable_var)
+#immutable_var[2] = 4 кортеж -это  неизменяемый тип данных (операция их изменения не поддерживается)
+mutable_list = [12, 'Urban', 2, '10', False]
+mutable_list[1:3] = 'University', 15
+print('Mutable_list: ', mutable_list)
